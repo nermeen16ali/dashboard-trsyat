f8ca0bffa4ab9a315aebe803fb1d7c63e04af7d0
nermeen16ali <nermeenali540@gmail.com>
2026-02-15 01:05:38 +0200

    d

--- js/dataTable.js
+++ js/dataTable.js
@@ -18,6 +18,22 @@
 // });
 
 $(document).ready(function () {
+  const isRTL = document.documentElement.dir === "rtl";
+
+  const rightArrow = `
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 21 20" fill="none">
+                        <path d="M12.5249 15.0586L17.5832 10.0002L12.5249 4.94189" stroke="#0C2030" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path opacity="0.4" d="M3.41675 10H17.4417" stroke="#0C2030" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+        `;
+
+  const leftArrow = `
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 21 20" fill="none">
+                        <path d="M8.4751 15.0586L3.41676 10.0002L8.4751 4.94189" stroke="#0C2030" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                        <path opacity="0.4" d="M17.5833 10H3.55825" stroke="#0C2030" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>
+        `;
+
   const table = $(".competition-table").DataTable({
     responsive: true,
     paging: true,
@@ -25,22 +41,13 @@ $(document).ready(function () {
     pageLength: 8,
     lengthChange: false,
     language: {
-      url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/ar.json",
+      url: isRTL
+        ? "//cdn.datatables.net/plug-ins/1.13.6/i18n/ar.json"
+        : "//cdn.datatables.net/plug-ins/1.13.6/i18n/en-GB.json",
       paginate: {
-        previous: `
-                  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
-                      <path d="M12.5249 15.0586L17.5832 10.0002L12.5249 4.94189" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                      <path opacity="0.4" d="M3.41675 10H17.4417" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-        `,
-        next: `
-        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
-                      <path d="M8.4751 15.0586L3.41676 10.0002L8.4751 4.94189" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                      <path opacity="0.4" d="M17.5833 10H3.55825" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-
-        `
-      }
+        previous: isRTL ? rightArrow : leftArrow,
+        next: isRTL ? leftArrow : rightArrow,
+      },
     },
   });
 
