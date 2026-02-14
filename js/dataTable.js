af08ad4cf6cc1981fd27f1ca1b0682941c6f54d5
nermeen16ali <nermeenali540@gmail.com>
2026-02-15 00:20:55 +0200

    d

--- js/dataTable.js
+++ js/dataTable.js
@@ -29,15 +29,15 @@ $(document).ready(function () {
       paginate: {
         previous: `
                   <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
-                        <path d="M12.5249 15.0586L17.5832 10.0002L12.5249 4.94189" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path opacity="0.4" d="M3.41675 10H17.4417" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                      </svg>
+                      <path d="M12.5249 15.0586L17.5832 10.0002L12.5249 4.94189" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                      <path opacity="0.4" d="M3.41675 10H17.4417" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
         `,
         next: `
-                  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
-                        <path d="M8.4751 15.0586L3.41676 10.0002L8.4751 4.94189" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                        <path opacity="0.4" d="M17.5833 10H3.55825" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
-                      </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
+                      <path d="M8.4751 15.0586L3.41676 10.0002L8.4751 4.94189" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                      <path opacity="0.4" d="M17.5833 10H3.55825" stroke="#0C2030" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
 
         `
       }
